9584f3b80d38103fbdd1c077b80113f8fa97a8dd
jillianneff <jilliannneff@gmail.com>
2020-11-14 18:49:46 -0500

    stack view set up

--- SuperContacts/ItemStore.swift
+++ SuperContacts/ItemStore.swift
@@ -21,4 +21,10 @@ class ItemStore {
             createItem()
         }
     }
+    
+    func removeItem(_ item: Item) {
+        if let index = allItems.firstIndex(of: item){
+            allItems.remove(at: index)
+        }
+    }
 }
--- SuperContacts/ItemsViewController.swift
+++ SuperContacts/ItemsViewController.swift
@@ -25,4 +25,49 @@ class ItemsViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive, handler: {(action) -> Void in
+                self.itemStore.removeItem(item)
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            
+            ac.addAction(deleteAction)
+            
+            present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func addNewItem(_ sender: UIButton){
+        let newItem = itemStore.createItem()
+        
+        if let index = itemStore.allItems.firstIndex(of: newItem){
+            let indexPath = IndexPath(row: index, section: 0)
+        
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton){
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        }
+        else {
+            sender.setTitle("Done", for: .normal)
+            setEditing(true, animated: true)
+        }
+        
+    }
+    
 }
